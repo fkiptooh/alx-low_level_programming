953487e029906814f4000a83af9ec01daabffce0
fkiptooh <fkiptooh@gmail.com>
2023-08-10 14:14:07 -0700

    Low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,5 @@
 #include <unistd.h>
-/**
- * main - Entry point 
-*/
+/* Function main is the entry point */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
