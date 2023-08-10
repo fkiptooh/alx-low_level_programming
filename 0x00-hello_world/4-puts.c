96ebde844003b77e6aa9c9f8311118ccc0326c8f
fkiptooh <fkiptooh@gmail.com>
2023-08-10 13:56:12 -0700

    Low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* main - A programs that prints a line using  puts function */
+/**
+ * main - A programs that prints a line using  puts function
+ * Return: 0 (provides sucess)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
