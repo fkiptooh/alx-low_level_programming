909bb5a05913ad0ad12f26aeee4d109b22e64952
fkiptooh <fkiptooh@gmail.com>
2023-08-11 10:09:43 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A programs that alphabets in lower cases
+ * Return:0 (success)
+*/
+int main(void)
+
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		putchar(lc);
+
+	putchar('\n');
+	return (0);
+}
